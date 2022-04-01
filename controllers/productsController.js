846c346e718cb3ac3c270a03d46d549508ad72f6
leandrochs <leandrochagask@gmail.com>
2022-04-01 00:27:31 -0300

    feat: adiciona controller para update

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,9 @@ const ProductsService = require('../services/productsService');
 const getAllProducts = async (req, res) => {
   try {
     const products = await ProductsService.getAllProducts();
-    if (!products) { return res.status(404).json({ message: 'Products not found' }); }
+    if (!products) {
+      return res.status(404).json({ message: 'Products not found' });
+    }
     return res.status(200).json(products);
   } catch (error) {
     console.log(error);
@@ -15,7 +17,9 @@ const getById = async (req, res) => {
   try {
     const { id } = req.params;
     const products = await ProductsService.getById(id);
-    if (!products) { return res.status(404).json({ message: 'Product not found' }); }
+    if (!products) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
     return res.status(200).json(products);
   } catch (error) {
     console.log(error);
@@ -26,11 +30,31 @@ const getById = async (req, res) => {
 const create = async (req, res) => {
   try {
     const product = await ProductsService.create(req.body);
-    if (product.error) { 
-      return res.status(product.error).json({ message: product.message }); 
+    if (product.error) {
+      return res.status(product.error).json({ message: product.message });
     }
 
     return res.status(201).json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro no Servidor' });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, quantity } = req.body;
+    const product = await ProductsService.update(id, name, quantity);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    if (product.error) {
+      return res.status(product.error).json({ message: product.message });
+    }
+
+    return res.status(200).json(product);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Erro no Servidor' });
@@ -41,4 +65,5 @@ module.exports = {
   getAllProducts,
   getById,
   create,
+  update,
 };
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -15,7 +15,7 @@ const getById = async (req, res) => {
   try {
     const { id } = req.params;
     const sales = await salesService.getById(id);
-    if (sales.length === 0) return res.status(404).json({ message: 'Sale not found' });
+    if (sales.length === 0) { return res.status(404).json({ message: 'Sale not found' }); }
     return res.status(200).json(sales);
   } catch (error) {
     console.log(error);
@@ -27,11 +27,28 @@ const create = async (req, res) => {
   try {
     const product = await salesService.create(req.body);
 
-    if (product.error) { 
-      return res.status(product.error).json({ message: product.message }); 
+    if (product.error) {
+      return res.status(product.error).json({ message: product.message });
     }
 
     return res.status(201).json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro no Servidor' });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { productId, quantity } = req.body[0];
+    const sale = await salesService.update(id, productId, quantity);
+
+    if (sale.error) {
+      return res.status(sale.error).json({ message: sale.message });
+    }
+
+    return res.status(200).json(sale);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Erro no Servidor' });
@@ -42,4 +59,5 @@ module.exports = {
   getAllSales,
   getById,
   create,
+  update,
 };
