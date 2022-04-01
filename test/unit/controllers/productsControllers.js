5a69c1a25d0988d222209bb39bb23e98e32365b0
leandrochs <leandrochagask@gmail.com>
2022-04-01 14:04:48 -0300

    feat: adiciona testes para  erros em controller Erros relacionados a parametros incorretos

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -114,5 +114,159 @@ describe("Produtos - (Camada Controller) Quando busca produtos por id", () => {
 
       expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
     });
+    });
+  });
+
+  describe("Quando cadastra um novo produto", () => {
+    describe("Quando 'name' possui menos que 5 caracteres ", async () => {
+      const response = {};
+      const request = {};
+
+      before(() => {
+        request.body = {
+          name: "prod",
+          quantity: 100,
+        };
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(ProductsService, "create").resolves({
+          error: 422,
+          message: '"name" length must be at least 5 characters long',
+        });
+      });
+
+      after(() => {
+        ProductsService.create.restore();
+      });
+
+      it('é chamado o método "status" passando 422', async () => {
+        await ProductsController.create(request, response);
+        expect(response.status.calledWith(422)).to.be.true;
+      });
+
+      it('é chamado o método "json" passando a mensagem name length must be at least 5 characters long', async () => {
+        await ProductsController.create(request, response);
+        expect(
+          response.json.calledWith({
+            message: '"name" length must be at least 5 characters long',
+          })
+        ).to.be.true;
+      });
+    });
+
+    describe("Quando a requisição é feita sem o atributo name", async () => {
+      const response = {};
+      const request = {};
+
+      before(() => {
+        request.body = {
+          quantity: 100,
+        };
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(ProductsService, "create").resolves({
+          error: 400,
+          message: '"name" is required',
+        });
+      });
+
+      after(() => {
+        ProductsService.create.restore();
+      });
+
+      it('é chamado o método "status" passando 400', async () => {
+        await ProductsController.create(request, response);
+        expect(response.status.calledWith(400)).to.be.true;
+      });
+
+      it('é chamado o método "json" passando a mensagem name is required', async () => {
+        await ProductsController.create(request, response);
+        expect(
+          response.json.calledWith({
+            message: '"name" is required',
+          })
+        ).to.be.true;
+      });
+    });
+
+    describe("Quando a requisição é feita sem o atributo quantity", async () => {
+      const response = {};
+      const request = {};
+
+      before(() => {
+        request.body = {
+          name: "produto",
+        };
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(ProductsService, "create").resolves({
+          error: 400,
+          message: '"quantity" is required',
+        });
+      });
+
+      after(() => {
+        ProductsService.create.restore();
+      });
+
+      it('é chamado o método "status" passando 400', async () => {
+        await ProductsController.create(request, response);
+        expect(response.status.calledWith(400)).to.be.true;
+      });
+
+      it('é chamado o método "json" passando a mensagem name is required', async () => {
+        await ProductsController.create(request, response);
+        expect(
+          response.json.calledWith({
+            message: '"quantity" is required',
+          })
+        ).to.be.true;
+      });
+    });
+
+    describe("Quando a requisição é feita com o quantity menor ou igual a 0", async () => {
+      const response = {};
+      const request = {};
+
+      before(() => {
+        request.body = {
+          name: "produto",
+          quantity: 0,
+        };
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(ProductsService, "create").resolves({
+          error: 422,
+          message: '"quantity" must be greater than or equal to 1',
+        });
+      });
+
+      after(() => {
+        ProductsService.create.restore();
+      });
+
+      it('é chamado o método "status" passando 422', async () => {
+        await ProductsController.create(request, response);
+        expect(response.status.calledWith(422)).to.be.true;
+      });
+
+      it('é chamado o método "json" passando a mensagem name is required', async () => {
+        await ProductsController.create(request, response);
+        expect(
+          response.json.calledWith({
+            message: '"quantity" must be greater than or equal to 1',
+          })
+        ).to.be.true;
+      });
+    });
+
   });
 });
