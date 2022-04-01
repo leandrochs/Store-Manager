a56c58f01c74f50981481c4f30dcc92c464eaa2c
leandrochs <leandrochagask@gmail.com>
2022-03-31 22:24:50 -0300

    feat: adiciona create em products model

--- models/productsModel.js
+++ models/productsModel.js
@@ -16,7 +16,30 @@ const getById = async (id) => {
   return products[0];
 };
 
+const create = async ({ name, quantity }) => {
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?);',
+    [name, quantity],
+  );
+
+  return {
+    id: insertId,
+    name,
+    quantity,
+  };
+};
+
+const getByName = async (name) => {
+  const [products] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE name = ?',
+    [name],
+  );
+  return products[0];
+};
+
 module.exports = {
   getAllProducts,
   getById,
+  create,
+  getByName,
 };
