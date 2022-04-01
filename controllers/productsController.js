126337a464b9fc256543b484ebfe6d543ea28933
leandrochs <leandrochagask@gmail.com>
2022-03-31 22:22:44 -0300

    feat: adiciona create em products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,7 @@ const ProductsService = require('../services/productsService');
 const getAllProducts = async (req, res) => {
   try {
     const products = await ProductsService.getAllProducts();
-    if (!products) return res.status(404).json({ message: 'Products not found' });
+    if (!products) { return res.status(404).json({ message: 'Products not found' }); }
     return res.status(200).json(products);
   } catch (error) {
     console.log(error);
@@ -15,8 +15,22 @@ const getById = async (req, res) => {
   try {
     const { id } = req.params;
     const products = await ProductsService.getById(id);
-    if (!products) return res.status(404).json({ message: 'Product not found' });
+    if (!products) { return res.status(404).json({ message: 'Product not found' }); }
     return res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro no Servidor' });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const product = await ProductsService.create(req.body);
+    if (product.error) { 
+      return res.status(product.error).json({ message: product.message }); 
+    }
+
+    return res.status(201).json(product);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Erro no Servidor' });
@@ -26,4 +40,5 @@ const getById = async (req, res) => {
 module.exports = {
   getAllProducts,
   getById,
+  create,
 };
