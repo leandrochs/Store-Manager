2d095b4a340aedf0ce759e681a18b7b092752a07
leandrochs <leandrochagask@gmail.com>
2022-04-03 12:41:45 -0300

    feat: teste em Service para cadastro de produto

--- test/unit/services/productsServices.js
+++ test/unit/services/productsServices.js
@@ -5,94 +5,158 @@ const connection = require("../../../models/connection");
 const ProductsService = require("../../../services/productsService");
 const ProductsModel = require("../../../models/productsModel");
 
-describe("Produtos - (Camada Service) Quando busca todos os produtos", () => {
-  before(() => {
-    sinon.stub(ProductsModel, "getAllProducts").resolves([
-      {
-        id: 1,
-        name: "Martelo de Thor",
-        quantity: 10,
-      },
-      {
-        id: 2,
-        name: "Traje de encolhimento",
-        quantity: 20,
-      },
-      {
-        id: 3,
-        name: "Escudo do Capitão América",
-        quantity: 30,
-      },
-    ]);
-  });
-
-  after(() => {
-    ProductsModel.getAllProducts.restore();
-  });
-
-  it("retorna um array", async () => {
-    const response = await ProductsService.getAllProducts();
-    expect(response).to.be.an("array");
-  });
-
-  it("O array não está vazio", async () => {
-    const response = await ProductsService.getAllProducts();
-    expect(response).to.be.not.empty;
-  });
-
-  it("É um array de objetos", async () => {
-    const response = await ProductsService.getAllProducts();
-    expect(response[0]).to.be.an("object");
-  });
-
-  it("Objeto possui chaves id, name e quantity", async () => {
-    const response = await ProductsService.getAllProducts();
-    expect(response[0]).to.include.all.keys("id", "name", "quantity");
-  });
-});
-
-describe("Produtos - (Camada Service) Quando busca produtos por id", () => {
-  describe("quando não existe um produto com o id informado", () => {
-    before(async () => {
-      sinon.stub(ProductsModel, "getById").resolves();
-    });
-
-    after(async () => {
-      ProductsModel.getById.restore();
-    });
-
-    it("retorna undefined", async () => {
-      const response = await ProductsService.getById();
-      expect(response).to.be.undefined;
-    });
-  });
-
-  describe("quando existe um produto com o id informado", () => {
+describe("(Camada Service de products - Produtos)", () => {
+  describe("Quando busca todos os produtos", () => {
     before(() => {
-      sinon.stub(ProductsModel, "getById").resolves({
-        id: 1,
-        name: "Martelo de Thor",
-        quantity: 10,
-      });
+      sinon.stub(ProductsModel, "getAllProducts").resolves([
+        {
+          id: 1,
+          name: "Martelo de Thor",
+          quantity: 10,
+        },
+        {
+          id: 2,
+          name: "Traje de encolhimento",
+          quantity: 20,
+        },
+        {
+          id: 3,
+          name: "Escudo do Capitão América",
+          quantity: 30,
+        },
+      ]);
     });
 
     after(() => {
-      ProductsModel.getById.restore();
+      ProductsModel.getAllProducts.restore();
     });
 
-    it("retorna um objeto", async () => {
-      const response = await ProductsService.getById(1);
-      expect(response).to.be.an("object");
+    it("retorna um array", async () => {
+      const response = await ProductsService.getAllProducts();
+      expect(response).to.be.an("array");
     });
 
-    it("O objeto não está vazio", async () => {
-      const response = await ProductsService.getById(1);
+    it("O array não está vazio", async () => {
+      const response = await ProductsService.getAllProducts();
       expect(response).to.be.not.empty;
     });
 
+    it("É um array de objetos", async () => {
+      const response = await ProductsService.getAllProducts();
+      expect(response[0]).to.be.an("object");
+    });
+
     it("Objeto possui chaves id, name e quantity", async () => {
-      const response = await ProductsService.getById(1);
-      expect(response).to.include.all.keys("id", "name", "quantity");
+      const response = await ProductsService.getAllProducts();
+      expect(response[0]).to.include.all.keys("id", "name", "quantity");
+    });
+  });
+
+  describe("Quando busca produtos por id", () => {
+    describe("Quando não existe um produto com o id informado", () => {
+      before(async () => {
+        sinon.stub(ProductsModel, "getById").resolves();
+      });
+
+      after(async () => {
+        ProductsModel.getById.restore();
+      });
+
+      it("retorna undefined", async () => {
+        const response = await ProductsService.getById();
+        expect(response).to.be.undefined;
+      });
+    });
+
+    describe("Quando existe um produto com o id informado", () => {
+      before(() => {
+        sinon.stub(ProductsModel, "getById").resolves({
+          id: 1,
+          name: "Martelo de Thor",
+          quantity: 10,
+        });
+      });
+
+      after(() => {
+        ProductsModel.getById.restore();
+      });
+
+      it("retorna um objeto", async () => {
+        const response = await ProductsService.getById(1);
+        expect(response).to.be.an("object");
+      });
+
+      it("O objeto não está vazio", async () => {
+        const response = await ProductsService.getById(1);
+        expect(response).to.be.not.empty;
+      });
+
+      it("Objeto possui chaves id, name e quantity", async () => {
+        const response = await ProductsService.getById(1);
+        expect(response).to.include.all.keys("id", "name", "quantity");
+      });
+    });
+  });
+
+  describe("Quando cadastra um produto", () => {
+    describe("Quando a requisição é feita com o atributo name igual um já cadastrado", () => {
+      before(async () => {
+        sinon.stub(ProductsModel, "getByName").resolves({
+          id: 1,
+          name: "Martelo de Thor",
+          quantity: 10,
+        });
+      });
+
+      after(async () => {
+        ProductsModel.getByName.restore();
+      });
+
+      it("retorna um objeto com as chaves 'error' e 'message'", async () => {
+        const response = await ProductsService.create({
+          name: "Martelo de Thor",
+          quantity: 10,
+        });
+        expect(response).to.include.all.keys("error", "message");
+      });
+    });
+
+    describe("Quando o cadastro é realizado", () => {
+      before(() => {
+        sinon.stub(ProductsModel, "create").resolves({
+          id: 1,
+          name: "Novo Produto",
+          quantity: 10,
+        });
+      });
+
+      after(() => {
+        ProductsModel.create.restore();
+      });
+
+      it("retorna um objeto", async () => {
+        const response = await ProductsService.create({
+          name: "Novo Produto",
+          quantity: 10,
+        });
+        expect(response).to.be.an("object");
+      });
+
+      it("O objeto não está vazio", async () => {
+        const response = await ProductsService.create({
+          name: "Novo Produto",
+          quantity: 10,
+        });
+        expect(response).to.be.not.empty;
+      });
+
+      it("Objeto possui chaves id, name e quantity", async () => {
+        const response = await ProductsService.create({
+          name: "Novo Produto",
+          quantity: 10,
+        });
+        expect(response).to.include.all.keys("id", "name", "quantity");
+      });
     });
   });
 });
